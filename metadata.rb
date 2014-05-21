ed5ae4d62c27c9718cd0385bc7c395ed6084a4a6
c-styles <eisuke@c-styles.jp>
2014-05-21 14:32:25 +0900

    rename to webmin

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'bastion-cookbook'
+name             'webmin'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
-description      'Installs/Configures bastion-cookbook'
+description      'Installs/Configures webmin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
