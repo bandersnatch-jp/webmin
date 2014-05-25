2edcf4794a32af4af938f812679782a7e714f877
c-styles <eisuke@c-styles.jp>
2014-05-25 16:41:41 +0900

    add recipe

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,10 @@ description      'Installs/Configures webmin'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe           'webmin', 'do all'
+recipe           'webmin::install', 'install'
+recipe           'webmin::repositry', 'create repo'
+
+
 depends          'apt'
 depends          'yum'
